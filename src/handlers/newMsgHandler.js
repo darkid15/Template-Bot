8d29d7b85f304b98244547c15ec4ff559f7843e7
Darkid15 <oluwapemisireoladele15@gmail.com>
2026-05-06 22:56:20 +0100

    Removed ai logic from message handlers.

--- src/handlers/newMsgHandler.js
+++ src/handlers/newMsgHandler.js
@@ -5,9 +5,6 @@ import analyzeObject from '../utils/message/analyze.js';
 import sendReply from '../utils/message/sendReply.js';
 import { info, warn, error } from '../utils/logger.js';
 import { readJSON } from '../utils/json.js';
-import askDark from '../utils/darkAi/groq.js';
-import { darkFile } from '../configs/paths.js';
-import { getTargetUser } from '../utils/message/tags.js';
 import {
     botName,
     prefix
@@ -18,29 +15,11 @@ export default async function handleNewMessages (sock, m, cmds, settings) {
     const { master } = settings;
     const userId = m.key.participant || m.key.remoteJid;
     const cleanId = userId?.split("@")[0] || "unknown";
-    let darkConfigs;
     try {
         if (!m.message) return warn("No message object detected!");
         // extract text 
         const text = await extractText(m);
         if (!text) return;
-        
-        const isCommand = text.startsWith(prefix);
-        if (!isCommand) darkConfigs = await readJSON(darkFile, { active: "false" }) || { active: "false" };
-        if (darkConfigs?.active=== "true" && !isCommand) {
-            const botLid = sock.user.lid.split(":")[0];
-            await analyzeObject(sock.user);
-            info(`Bot number: ${botLid}`);
-            const mentionedJids = await getTargetUser(m, true) || [];
-            info(`Mentioned Jids: ${mentionedJids || "None"}`);
-            const isTagged = mentionedJids.includes(`${botLid}@lid`);
-            const hasDarkWord = /\bdark\b/i.test(text);
-            if (isTagged || hasDarkWord) {
-                const reply = await askDark(cleanId, text) || ":error: Chatbot error. Try again later...";
-                return sendReply(sock, m, reply);
-            }
-        }
-        
         if (!text.startsWith(prefix)) return;
         
         // remove prefix to get singular command
@@ -49,11 +28,7 @@ export default async function handleNewMessages (sock, m, cmds, settings) {
         
         if (!cmdName) return;
         const cmd = commands.get(cmdName) || aliases.get(cmdName);
-        if (!cmd) {
-            warn(`No such command: ${cmdName}`);
-            await sendReply(sock, m, ":error: No such command! Baka!");
-            return;
-        }
+        if (!cmd) return;
         try {
             cmd.execute({
                 sock,       // type: Object 
