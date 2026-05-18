c693134c638736da1c40bea0124abc847abf00ce
Darkid15 <oluwapemisireoladele15@gmail.com>
2026-05-18 22:28:17 +0100

    Added try-except blockt wrap full bot starter function

--- src/index.js
+++ src/index.js
@@ -3,34 +3,30 @@
 import createSocket from './socket/index.js';
 import handleSocketEvents from './socket/events/index.js'
 import { info, error } from './utils/logger.js';
-import { 
-    BOT_NAME,
-    BOT_PHONE,
-    MASTER_PHONE 
-} from './configs/index.js';
+import { getBotCfgs } from './configs/index.js';
 
 async function startBot () {
+    let botName = "Template-Bot";
     try {
-        info("Starting Template Bot...")
+        const bot = await getBotCfgs();
+        botName = bot?.self?.name;
+        info(`Starting ${botName}...`)
         const sock = await createSocket("auth");    // Creates a folder in the project root directory named "auth". 
         
         const isRegistered = sock.authState.creds.registered;
-        
         /*
-            Handles socket events:
-              - sock.ev.on("connection.update", ()=>{...}) 
-              - sock.ev.on("messages.upsert", ()=>{...})  
+          Handles socket events:
+            - sock.ev.on("connection.update", ()=>{...}) 
+            - sock.ev.on("messages.upsert", ()=>{...})  
         */
         await handleSocketEvents(sock, {
             pairCodeLogin: true,
-            phone: BOT_PHONE || null,
             reconnect: startBot,
-            isRegistered: isRegistered,
-            master: MASTER_PHONE || BOT_PHONE
+            isRegistered,
+            bot 
         });
-        if (sock) info(JSON.stringify(sock))
     } catch (err) {
-        error(`Error starting ${BOT_NAME}: ${err.stack || err.message || err}`)
+        error(`:exclaim2: Error starting ${botName}: ${err.stack || err.message || err}`)
         process.exit(1)
     }
 }
